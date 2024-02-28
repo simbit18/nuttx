5cddef28ee400d876649289a45a3acc98e0be1e8
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-02-28 16:38:55 +0100

    Update ubuntu.sh

--- tools/ci/platforms/ubuntu.sh
+++ tools/ci/platforms/ubuntu.sh
@@ -239,7 +239,7 @@ rust() {
   echo "export RUSTUP_HOME=${tools}/rust/rustup" >> "${tools}"/env.sh
   if ! type rustc > /dev/null 2>&1; then
     # Install Rust target x86_64-unknown-linux-musl
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --no-modify-path -y
     # Install targets supported from NuttX
     "$CARGO_HOME"/bin/rustup target add thumbv6m-none-eabi
     "$CARGO_HOME"/bin/rustup target add thumbv7m-none-eabi
