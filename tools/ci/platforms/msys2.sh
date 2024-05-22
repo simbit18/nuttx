a3773927bacb3eabff75a6773170d1a5009ed7b1
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-05-22 14:20:44 +0200

    add board

--- tools/ci/platforms/msys2.sh
+++ tools/ci/platforms/msys2.sh
@@ -281,7 +281,7 @@ install_build_tools() {
   mkdir -p "${NUTTXTOOLS}"
   echo "#!/usr/bin/env sh" > "${NUTTXTOOLS}"/env.sh
 
-  install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain kconfig_frontends riscv_gcc_toolchain rust"
+  install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain esp-tool gen-romfs kconfig_frontends riscv_gcc_toolchain rust xtensa-esp32-gcc-toolchain"
 
   oldpath=$(cd . && pwd -P)
   for func in ${install}; do
