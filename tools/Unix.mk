6cbe60f8d326b56b5027daec2067612d61934f9b
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-07-26 18:32:32 +0200

    Update Unix.mk

--- tools/Unix.mk
+++ tools/Unix.mk
@@ -19,6 +19,15 @@
 ############################################################################
 
 export TOPDIR := ${shell echo $(CURDIR) | sed -e 's/ /\\ /g'}
+WSDIR := ${shell cd "${TOPDIR}"/.. && pwd -P}
+
+export NXTMPDIR := $(WSDIR)/nxtmpdir
+$(info $$NXTMPDIR = $(NXTMPDIR))
+
+ifeq ($(wildcard $(NXTMPDIR)),)
+$(shell mkdir -p $(NXTMPDIR))
+$(info "nxtmpdir folder created !!!")
+endif
 
 ifeq ($(V),)
   MAKE := $(MAKE) -s --no-print-directory
