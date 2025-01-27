98704dad2c7ecba24bf95483a392d08c2094c722
simbit18 <101105604+simbit18@users.noreply.github.com>
2025-01-27 11:32:48 +0100

    Update testbuild.ps1

--- tools/testbuild.ps1
+++ tools/testbuild.ps1
@@ -93,8 +93,10 @@ function Find-VisualStudio {
 
     if ($MSVC -eq 1) {
         Write-Output "Found Visual Studio 2022 installations"
+        return $MSVC=1
     } else {
         Write-Output "Visual Studio 2022 is not installed on this system."
+        return $MSVC=0
     }
 
 }
