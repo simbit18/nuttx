eb8b257cee4722dfd1040637a0b9d65e4a5e0200
simbit18 <simbit18@gmail.com>
2025-05-29 15:42:10 +0200

    Update cygwin.sh

--- tools/ci/platforms/cygwin.sh
+++ tools/ci/platforms/cygwin.sh
@@ -293,8 +293,8 @@ setup_links() {
 install_build_tools() {
   mkdir -p "${NUTTXTOOLS}"
   echo "#!/usr/bin/env sh" > "${NUTTXTOOLS}"/env.sh
-
-  install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain kconfig_frontends riscv_gcc_toolchain rust python_tools"
+  install="arm_gcc_toolchain kconfig_frontends"
+  # install="arm_clang_toolchain arm_gcc_toolchain arm64_gcc_toolchain kconfig_frontends riscv_gcc_toolchain rust python_tools"
 
   oldpath=$(cd . && pwd -P)
   for func in ${install}; do
